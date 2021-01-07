cc8e7586a4017c199a4a391800d4fc81bece622e
SANOGO <SOULEYSANOGO@gs2e.ci>
2021-01-07 16:07:08 +0000

    Update

--- src/main/java/com/techprimers/repository/UserJpaRespository.java
+++ src/main/java/com/techprimers/repository/UserJpaRespository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@Component
 @RepositoryRestResource
 public interface UserJpaRespository extends JpaRepository<User, Long>{
 
