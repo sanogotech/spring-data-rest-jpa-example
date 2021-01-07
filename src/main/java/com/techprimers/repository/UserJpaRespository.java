413f90859f341aa4be64c3d09b0d6d1f46c5c044
SANOGO <SOULEYSANOGO@gs2e.ci>
2021-01-07 15:51:03 +0000

    Update

--- src/main/java/com/techprimers/repository/UserJpaRespository.java
+++ src/main/java/com/techprimers/repository/UserJpaRespository.java
@@ -3,9 +3,19 @@ package com.techprimers.repository;
 import com.techprimers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Component
+//@Component
+@RepositoryRestResource
 public interface UserJpaRespository extends JpaRepository<User, Long>{
 
     User findByName(String name);
+	
+	/*
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+	*/
+
 }
